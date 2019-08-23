a95a53e7832fbbc6a2faf0a5a8606f5fc50e66d4
gnudrew <gnudrew@gmail.com>
2019-08-23 01:03:26 -0500

    some exercises

--- philosopher.py
+++ philosopher.py
@@ -1,6 +1,11 @@
 """
 An example of the deadlock-philosopher problem we talked about.  Needs fixing!
+
+8/23/2019, ASR
+Solution: Let one of the philosophers be dyslexic. He always picks up his right fork first, then his left.
+See "notepadPhilosopher.txt" for further notes and discussion of my thought process.
 """
+
 
 # For the purpose of this example, a fork will literally be a threading.Lock
 # object but thematically it makes more sense in context of what we talked about
@@ -10,10 +15,10 @@ from threading import Thread
 from time import sleep, time
 
 SEATS_PER_TABLE = 5
-MEALS = 100                 # Hmmm, why does it never seem to end when I
+MEALS = 1000                 # Hmmm, why does it never seem to end when I
                             # increase this number?  When I make it smaller, it
                             # the program seems to terminate quicker...
-MEAL_CONSUMPTION_TIME = 0   # Hmmm, why does it never seem to end when I
+MEAL_CONSUMPTION_TIME = 0.01   # Hmmm, why does it never seem to end when I
                             # increase this number to 0.1?
 
 class Plate(object):
@@ -37,6 +42,24 @@ class Plate(object):
         # but it is essentially equivalent to eat_v1()
         with self.left:
             with self.right:
+                #sleep(MEAL_CONSUMPTION_TIME)  # we should spend time eating here, too, right?
+                self.meals_eaten += 1
+
+
+    def eat_v1_dyslexic(self):
+    # Used only and always by the fifth philosopher, who is dyslexic. He takes right fork first, then left.
+        self.right.acquire()
+        self.left.acquire()
+        sleep(MEAL_CONSUMPTION_TIME)
+        self.meals_eaten += 1
+        self.left.release()
+        self.right.release()
+
+    def eat_v2_dyslexic(self):
+    # Used only and always by the fifth philosopher, who is dyslexic. He takes right fork first, then left.
+        with self.right:
+            with self.left:
+                #sleep(MEAL_CONSUMPTION_TIME) # we should spend time eating here, too, right?
                 self.meals_eaten += 1
 
 class Philosoper(Thread):
@@ -46,9 +69,15 @@ class Philosoper(Thread):
         self.plate = None
 
     def run(self):
-        for meal in range(MEALS//2):
-            self.plate.eat_v1()
-            self.plate.eat_v2()
+        if self.number == 1:
+            # philosopher 1 is dyslexic
+            for meal in range(MEALS//2):
+                self.plate.eat_v1_dyslexic()
+                self.plate.eat_v2_dyslexic()
+        else:
+            for meal in range(MEALS//2):
+                self.plate.eat_v1()
+                self.plate.eat_v2()
 
 
 class Table(object):
@@ -80,6 +109,7 @@ class Table(object):
 
     def go(self):
         start_time = time()
+
         # Give everyone a plate and dig in
         for eater, plate in zip(self._seats, self._plates):
             eater.plate = plate
