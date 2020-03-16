c10142fdff0b94af4dbdbb41ee0c36cdc399564a
gnudrew <gnudrew@gmail.com>
2020-03-15 23:39:49 -0500

    some updates

---
+++ Project3 - Destigmafier/script1.py
@@ -0,0 +1,8 @@
+"""
+The app scans input text and replaces stigma-inducing "trigger" words. The user can add, search, replace, update word replacements in the library.
+The resulting text should make the reader laugh rather than get triggered, by simply replacing words illustrating the silliness of it all, Morrrrrtyyyy.
+"""
+
+from tkinter import *
+import json
+import difflib
